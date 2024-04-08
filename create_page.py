81b56a62fc4184277395ac3a6a12e76338e0eba1
Jessica Nash <janash@vt.edu>
2024-04-08 15:50:27 -0400

    more updates to mdi standard documentation

--- create_page.py
+++ create_page.py
@@ -11,17 +11,10 @@ def load_standard():
     # Load the MDI standard
     with open("api/mdi_standard/mdi_standard.yaml", "r", encoding='utf-8') as f:
         mdi_standard = yaml.safe_load(f)
-    
-    mdi_standard = mdi_standard["commands"]
-
-    # Sort the temporary dictionary by its keys and insert into an OrderedDict
-    ordered_standard = OrderedDict(sorted(mdi_standard.items()))
-
-    return ordered_standard
+    return mdi_standard["commands"]
 
 def group_commands(standard):
-    """Groups sending and receiving commands together"""
-
+    """Groups sending and receiving commands together and then sorts them alphabetically"""
     grouped_standard = {}
 
     for command, command_info in standard.items():
@@ -34,11 +27,17 @@ def group_commands(standard):
         
         if command_group not in grouped_standard:
             grouped_standard[command_group] = []
-            grouped_standard[command_group].append(command_info)    
-        else:
-            grouped_standard[command_group].append(command_info)
+        
+        grouped_standard[command_group].append(command_info)
 
-    return grouped_standard
+    # Sorting each group's commands alphabetically by their 'name' key
+    for group in grouped_standard:
+        grouped_standard[group] = sorted(grouped_standard[group], key=lambda x: x['name'])
+    
+    # Sorting the groups alphabetically and returning an OrderedDict
+    ordered_grouped_standard = OrderedDict(sorted(grouped_standard.items()))
+    
+    return ordered_grouped_standard
 
 
 def create_page(command_name, command_list):
@@ -53,10 +52,10 @@ def create_page(command_name, command_list):
 
         if command_dict.get("datatype"):
             page_text += f"\n**Datatype:** `{command_dict['datatype']}`  "
-            page_text += f"\n**Quantity**: `{command_dict['count']}`"
+            page_text += f"\n**Quantity**: `{command_dict['count']}`  "
 
         if command_dict.get("format"):
-            page_text += f"\n**Format:** `{command_dict['format']}`"
+            page_text += f"\n**Format:** {command_dict['format']}"
 
         page_text += f"\n\n {command_dict.get('doc')}"
 
